e4ff6d543dd80b713080ab06d280564169809e1a
422legendary <zsr142857xyz@qq.com>
2025-08-15 19:33:59 +0800

    20250815 added

---
+++ codes/cache/20250815/test-all.c
@@ -0,0 +1,79 @@
+1.324e+3+3
+abc+.1-e
+2e-3L+e2
+2E+32345
+.1e3
+34534.34F+2
+System.out.println("hello, world\n");
+"\\\"" "wef\
+" + wef
+switch()case'a':default:break;
+public static void main(String[] args) {
+	
+}
+#define ABCD \
+lll \
+wef
+int *p;
+#define GREATER(a, b, result) ({ \
+	asm("cmp %1, %2\n\t" \
+		"jge 0f\n\t" \
+		"movl %1, %0\n\t" \
+		"jmp 1f\n " \
+		"0:\n\t" \
+		"movl %2, %0\n " \
+		"1:" \
+		:"=r"(result) /* comments */\
+		:"r"(a), "r"(b)); })
+CODESG ENDS cout << "hello world" << endl;
+#if\
+asdf
+
+a=100 # awpeignwaeg
+b=50 #2t23t
+print(a+b,end="=c")
+0x3453+1
+0xE+10
+0xF+1
+R"(ab
+c)"
+int main
+	int a b c using namespace std and   bye bye
+auto main() -> int
+print "hello cmr"
+a is 'A' and b is "b
+#include "stdio.h"
+int main(){return 0;}
+scanf("%d", &a);
+char l[1000], *p1=l, *p2, t;
+'wawawaw
+0b'2233423'32323 // C++14 digital separator
+'\n\'\"\\\\\"'"
+0x35f 324 88658758758575ulll sizeof 'd' 'f' 088ull 0x
+#if 0
+#define asdf ee\
+weggwrh\h
+reherh
+// 232trt\
+wetwet
+"wef\
+ gewg"
+  /* ÖÐÎÄ */ 
+#endif
+"asdf\
+325\h
+#include /* 
+ */ <iostream>
+  5g34g
+  #include //\
+23f32f32g
+23g23g
+#endif
+'
+/*  *
+ert */
+#define asdf\
+awer
+int a; //I'm a comment
+
+return b; ///triple slash introduces a comment too
